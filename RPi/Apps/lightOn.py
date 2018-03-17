11630909c46d78f409585b720e25aa48c0117689
Sanbee <bhatnagar.sanjay@gmail.com>
2018-03-16 21:03:48 -0600

    Cleaned-up the python apps.
    Load the mysockect module from NaaradServer + made them exectuable commandline commands.Z

--- RPi/Apps/lightOn.py
+++ RPi/Apps/lightOn.py
@@ -1,24 +1,27 @@
-# import socket;
-# from socket import error as SocketError
-# import errno
+#! /usr/bin/python
+import sys
+sys.path.insert(0, '../NaaradServer/NewServer');
+
 from mySock import mysocket;
 import time;
-import sys
 
 SERVER="raspberrypi";
 PORT=1234;
 
 def main(argv):
-    LAMP=sys.argv[1];
-    OP=sys.argv[2];
-    CMD="tell "+LAMP+" "+OP;
+    if (len(sys.argv) < 3):
+	print("Usage: "+sys.argv[0]+" LAMP 0|1");
+    else:
+    	LAMP=sys.argv[1];
+    	OP=sys.argv[2];
+    	CMD="tell "+LAMP+" "+OP;
 
-    naaradSoc=mysocket();
-    naaradSoc.connect(SERVER,PORT);
-    naaradSoc.send("open");time.sleep(1);
-    naaradSoc.send(CMD);time.sleep(1);
-    naaradSoc.send("done");time.sleep(1);
-    naaradSoc.close();
+    	naaradSoc=mysocket();
+    	naaradSoc.connect(SERVER,PORT);
+    	naaradSoc.send("open");time.sleep(1);
+    	naaradSoc.send(CMD);time.sleep(1);
+    	naaradSoc.send("done");time.sleep(1);
+    	naaradSoc.close();
 
 if __name__ == "__main__":
     main(sys.argv)
