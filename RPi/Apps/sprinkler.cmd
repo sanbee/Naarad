5a9a9116eb139eb4c4707e5748228b3d4391e6c2
Sanjay Bhatnagar <bhatnagar.sanjay@gmail.com>
2019-01-10 18:12:33 -0700

    Added comments.

--- RPi/Apps/sprinkler.cmd
+++ RPi/Apps/sprinkler.cmd
@@ -1,8 +1,8 @@
-./cmdnaarad.py RFM_SEND 16 4 10 1
-./cmdnaarad.py RFM_SEND 16 0 0 0 
-./cmdnaarad.py RFM_SEND 16 1 0 0 
-./cmdnaarad.py RFM_SEND 16 0 0 0 
-./cmdnaarad.py RFM_SEND 16 4 60 1
+./cmdnaarad.py RFM_SEND 16 4 10 1 # Set ping timeout to 10 sec
+./cmdnaarad.py RFM_SEND 16 1 0 0  # Open the valve
+sleep 60
+./cmdnaarad.py RFM_SEND 16 0 0 0  # Close the valve
+./cmdnaarad.py RFM_SEND 16 4 60 1 # Set ping timeout to 60 sec
 
 #./cmdnaarad.py RFM_SEND 16 4 60 1 
 #./cmdnaarad.py RFM_SEND 16 4 10 1 
