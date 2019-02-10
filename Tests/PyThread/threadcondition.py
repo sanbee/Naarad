ca5184cb590b5dfcdd29b8e6d85e77b8445537f4
Sanjay Bhatnagar <bhatnagar.sanjay@gmail.com>
2019-02-09 21:44:54 -0700

    Hopefully a thread-safe version with multiple client threads registered with the main thread.

--- Tests/PyThread/threadcondition.py
+++ Tests/PyThread/threadcondition.py
@@ -1,42 +1,102 @@
 import sys;
 import time;
 import signal
-from threading import Thread, Condition, Event;
+import threading;
+from threading import Thread, Condition, Event, RLock;
 
 condition = Condition();
 counter=0;
-NotifyClient={};
-NotifyClient['id']=[];
-NotifyClient['cond']=[];
 
-def findItem(aItem, aList):
-    indices = [];
-    n=len(aList);
-    for i in range(0,n):
-        if (aItem == aList[i]):
-            indices.append(i);
-    return indices;
+class MyList(list):
+    def __init__(self):
+        self.lock = RLock()
 
-def NaaradNotify(nodeid=-1):
-    global NotifyClient;
-    #Notify all register threads
-    if (nodeid < 0):
-        for i in range(len(NotifyClient['id'])):
-            c=NotifyClient['cond'][i];
-            c.acquire();
-            c.notify();
-            c.release();
-    else:
-        #Notify all registered threads with nodeid;
-        ndx=findItem(nodeid,NotifyClient['id']);
-        n = len(ndx);
-        for i in range(0,n):
-            c=NotifyClient['cond'][ndx[i]];
-            c.acquire();
-            c.notify();
-            c.release();
+    def append(self, val):
+        self.lock.acquire()
+        try:
+            list.append(self, val)
+        finally:
+            self.lock.release()
+
+    def remove(self,index):
+        self.lock.acquire();
+        try:
+            del self[index];
+        finally:
+            self.lock.release();
+
+    def findItem(self,aItem):
+        self.lock.acquire();
+        try:
+            indices = [];
+            n=len(self);
+            for i in range(0,n):
+                if (aItem == self[i]):
+                    indices.append(i);
+        finally:
+            self.lock.release();
+        return indices;
+
+#
+#------------------------------------Global functions----------------------------------------
+#
+class ClientList():
+    def __init__(self):
+        self.IDList = MyList();
+        self.CondList = MyList();
+        self.rlock = RLock();
+
+    def getIDList(self):
+        return self.IDList;
         
+    def getCondList(self):
+        return self.CondList;
 
+    def NaaradNotify(self,nodeid=-1):
+        global NotifyClient;
+        #Notify all register threads
+        if (nodeid < 0):
+            with self.rlock:
+                n=len(self.CondList);
+                for i in range(n):
+                    c=self.CondList[i];
+                    c.acquire();
+                    c.notify();
+                    c.release();
+        else:
+            #Notify all registered threads with nodeid;
+            with self.rlock:
+                ndx=self.IDList.findItem(nodeid);
+                n = len(ndx);
+                for i in range(0,n):
+                    c=self.CondList[ndx[i]];
+                    c.acquire();
+                    c.notify();
+                    c.release();
+        
+    def register(self,thisID,cond):
+        myIndex=-1;
+        with self.rlock:
+            myIndex=len(self.IDList);
+            self.IDList.append(thisID);
+            self.CondList.append(cond);
+        return myIndex;
+
+    def unregister(self,myCond):
+        with self.rlock:
+            try:
+                myIndex=self.CondList.findItem(myCond);
+                print 'Unregistering ',self.IDList[myIndex[0]];
+                self.IDList.remove(myIndex[0]);
+                self.CondList.remove(myIndex[0]);
+            except IndexError:
+                print "IndexError: ",self.IDList, myIndex;
+
+
+NotifyClient=ClientList();
+#
+#------------------------------------Global functions----------------------------------------
+#
 class mainThread(Thread):
     def __init__(self):
         Thread.__init__(self)
@@ -56,6 +116,7 @@ class mainThread(Thread):
     def exit(self):
         global counter;
         counter=-10;
+        NotifyClient.NaaradNotify();
 
     def run(self):
         global counter, NotifyCounter;
@@ -63,7 +124,7 @@ class mainThread(Thread):
         self.resume();
         while(True):
             if (counter < 0):
-                NaaradNotify();
+#                NaaradNotify();
                 print "Exiting MT";
                 break;
             with self.state:
@@ -73,33 +134,28 @@ class mainThread(Thread):
             time.sleep(2);
             counter += 1;
             #            if (counter%10==0):
-            if (counter in NotifyClient['id']):
-                NaaradNotify(counter);
+            if (counter in NotifyClient.getIDList()):
+                NotifyClient.NaaradNotify(counter);
 
 
 class clientThread(Thread):
-    def __init__(self,myid):
-        global ClientNotification;
+    def __init__(self,id):
+        global ClientList;
         Thread.__init__(self)
         self.cond=Condition();
-        self.id=myid;
-        self.register(self.id,self.cond);
+        self.myid=id;
+        self.myIndex = -1;
+        self.myIndex = NotifyClient.register(self.myid,self.cond);
 
-    def register(self,id,cond):
-        NotifyClient['id'].append(id);
-        NotifyClient['cond'].append(cond);
-
-    def unregister(self):
-        (NotifyClient['id']).remove(self.id);
-        (NotifyClient['cond']).remove(self.cond);
-        
+    def getCondition(self):
+        return self.cond;
 
     def run(self):
-        global counter;
+        global counter, ClientList;
         while(True):
             self.cond.acquire();
             self.cond.wait();
-            print "CT: ",self.id,counter;
+            print "CT: ",self.myid,counter;
             self.cond.release();
 
             # self.conditionEvent.acquire();
@@ -107,9 +163,9 @@ class clientThread(Thread):
             # print "CT: ",counter;
             # self.conditionEvent.release();
             if ((counter<0) or (counter>=20)):
-                print "CT exiting ",self.id;
+                print "CT exiting ",self.myid;
                 break;
-            self.unregister();
+        NotifyClient.unregister(self.cond);
 
 t0=mainThread();
 t1=clientThread(10);
