04c5cc66a8475c6131f875b34e78acc890a280b8
Sanjay Bhatnagar <bhatnagar.sanjay@gmail.com>
2019-02-15 22:40:21 -0700

    Various bug fixes in notify + a script to start and stop the sprinkler that uses notify.

--- RPi/Apps/notify.py
+++ RPi/Apps/notify.py
@@ -16,33 +16,41 @@ class MyException(Exception):
 
 def main(argv):
         if (len(sys.argv) < 7):
-		print "Usage: "+sys.argv[0]+" notify NODEID CMD SOURCE TIMEOUT nRETRIALS\n";
+            print "Usage: "+sys.argv[0]+" notify NODEID CMD SOURCE TIMEOUT nRETRIALS\n";
         else:
-                try:
-		        naaradcmd=sys.argv[1];
-                        nodeid=sys.argv[2]
-                        cmd=sys.argv[3];
-                        src=sys.argv[4];
-		        for i in range(2,len(sys.argv)):
-                            naaradcmd=naaradcmd+" "+sys.argv[i]
+            try:
+                naaradcmd=sys.argv[1];
+                nodeid=sys.argv[2]
+                cmd=sys.argv[3];
+                src=str(sys.argv[4]);
+                for i in range(2,6):
+                    naaradcmd=naaradcmd+" "+str(sys.argv[i]);
 
-		        FULLCMD=naaradcmd;
+                nRETRIALS=int(sys.argv[6]);
 
-		        print FULLCMD;
-		        naaradSoc=mysocket();
-                        Retry=0;
-                        while (Retry < nRETRIALS):
-                            naaradSoc.connect(SERVER,PORT);
-                            naaradSoc.send("open");time.sleep(0.1);
-                            naaradSoc.send(FULLCMD);time.sleep(0.1);
-                            tt=naaradSoc.receive();
-                            print tt,"   : Trial ",Retry;
-                            jdict=json.loads(tt);
-                            if ((time.time()*1000 - jdict['time']) > 1000.0):
-                                Retry += 1;
-                            naaradSoc.send("done");time.sleep(0.1);
-                            naaradSoc.close();
-                except MyException as e:
-                        print str(e);
+                FULLCMD=naaradcmd;
+
+                print FULLCMD;
+                Retry=0;
+                while (Retry < nRETRIALS):
+                    naaradSoc=mysocket();
+                    naaradSoc.connect(SERVER,PORT);
+                    naaradSoc.send("open");time.sleep(0.1);
+                    naaradSoc.send(FULLCMD);time.sleep(0.1);
+                    tt=naaradSoc.receive();
+                    print tt,"   : Trial ",Retry;
+                    jdict=json.loads(tt);
+                    print time.time()*1000 - jdict['time'];
+
+                    if ((time.time()*1000 - jdict['time']) > 1500.0):
+                        Retry += 1;
+                    else:
+                        Retry = nRETRIALS+1;
+
+                        naaradSoc.send("done");time.sleep(0.1);
+                        naaradSoc.close();
+            except MyException as e:
+                print str(e);
+
 if __name__ == "__main__":
     main(sys.argv)
--- RPi/Apps/sprinkler.cmd
+++ RPi/Apps/sprinkler.cmd
@@ -1,7 +1,11 @@
 ./cmdnaarad.py RFM_SEND 16 4 10 1 # Set ping timeout to 10 sec
+sleep 0.1
 ./cmdnaarad.py RFM_SEND 16 1 0 0  # Open the valve
-sleep 60
+sleep 0.1
+./notify.py notify 16 01 ACKpkt: 120 2
+sleep 30
 ./cmdnaarad.py RFM_SEND 16 0 0 0  # Close the valve
+sleep 0.1
 ./cmdnaarad.py RFM_SEND 16 4 60 1 # Set ping timeout to 60 sec
 
 #./cmdnaarad.py RFM_SEND 16 4 60 1 
