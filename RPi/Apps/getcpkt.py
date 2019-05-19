18f417f648c28d436f46994ffda26ca7fdd0c6a4
Sanjay Bhatnagar <bhatnagar.sanjay@gmail.com>
2019-05-19 15:39:08 -0600

    Adding the apps to get the current packet.
    getckpt: For the real Naarad server
    sgetckpt: For the simulated Naarad server.  The only difference is the IP address it
    connects to!

---
+++ RPi/Apps/getcpkt.py
@@ -0,0 +1,28 @@
+#! /usr/bin/python
+import sys
+sys.path.insert(0, '../NaaradServer/NewServer');
+
+from mySock import mysocket;
+import time;
+
+SERVER="naaradhost";
+SERVER="192.168.0.66";
+PORT=1234;
+
+
+def main(argv):
+        if (len(sys.argv) < 2):
+		print "Usage: "+sys.argv[0]+" NODEID";
+        else:
+            CMD="getcpkt "+str(sys.argv[1]);		
+            soc=mysocket();
+            soc.connect(SERVER,PORT);
+            soc.send("open");time.sleep(0.1);
+            soc.send(CMD); time.sleep(0.1);
+            tt=soc.receive();
+            soc.send("done");time.sleep(0.1);
+            print tt;
+
+
+if __name__ == "__main__":
+    main(sys.argv)
