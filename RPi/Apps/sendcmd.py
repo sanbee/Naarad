db8ad2f696cbfb0eceaf466589de5d93b063c23d
Sanjay Bhatnagar <bhatnagar.sanjay@gmail.com>
2019-07-09 22:25:18 -0600

    Double check with the user if the "shutdown" command for the named server was really intended.  The "shutdown" command will shut the server on the named machine.

--- RPi/Apps/sendcmd.py
+++ RPi/Apps/sendcmd.py
@@ -5,12 +5,19 @@ from mySock import mysocket;
 import sys
 import time;
 
+def isYes(str):
+    print(str);
+    return (raw_input()=="yes");
+
 def main(argv):
     if (len(sys.argv) < 2):
 	print("Usage: cmd");
     else:
     	CMD=sys.argv[1];
-
+        if (CMD=="shutdown"):
+            if (not isYes("shutdown server at "+serverinfo.SERVER+"."+" Are you sure?")):
+                return;
+                
     	naaradSoc=mysocket();
     	naaradSoc.connect(serverinfo.SERVER,serverinfo.PORT);
     	naaradSoc.send("sendcmd App");
