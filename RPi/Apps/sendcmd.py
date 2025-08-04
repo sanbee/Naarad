d393f686caec47c41691bb9e528d369fe4446d5c
Sanjay Bhatnagar <bhatnagar.sanjay@gmail.com>
2025-08-03 19:19:25 -0600

    Made tabbing consistent in sendcmd.py

--- RPi/Apps/sendcmd.py
+++ RPi/Apps/sendcmd.py
@@ -13,26 +13,26 @@ def isYes(str):
 def main(argv):
     narg = len(sys.argv);
     if (narg < 2):
-	print("Usage: cmd [SERVER-IP] [SERVER-PORT]");
+        print("Usage: cmd [SERVER-IP] [SERVER-PORT]");
     else:
     	CMD=sys.argv[1];
-	SERVER_IP=serverinfo.SERVER;
-	SERVER_PORT=serverinfo.PORT;
+        SERVER_IP=serverinfo.SERVER;
+        SERVER_PORT=serverinfo.PORT;
         if (narg > 2):
-		SERVER_IP = sys.argv[2];
-	if (narg > 3):
-		SERVER_PORT = sys.argv[3];
+            SERVER_IP = sys.argv[2];
+        if (narg > 3):
+            SERVER_PORT = sys.argv[3];
 
         if (CMD=="shutdown"):
             if (not isYes("shutdown server at "+str(SERVER_IP)+":"+str(SERVER_PORT)+"." + " Are you sure?")):
                 return;
                 
-    	naaradSoc=mysocket();
-    	naaradSoc.connect(SERVER_IP,SERVER_PORT);
-    	naaradSoc.send("sendcmd App");
-    	naaradSoc.send(CMD);time.sleep(0.1);
-    	naaradSoc.send("done");
-    	naaradSoc.close();
+        naaradSoc=mysocket();
+        naaradSoc.connect(SERVER_IP,SERVER_PORT);
+        naaradSoc.send("sendcmd App");
+        naaradSoc.send(CMD);time.sleep(0.1);
+        naaradSoc.send("done");
+        naaradSoc.close();
 
 if __name__ == "__main__":
     main(sys.argv)
